c393640a082100041070db2046c5658e2c9eaf7a
elhadyLemtai <78748781+elhadyLemtai@users.noreply.github.com>
2024-05-28 13:10:34 +0100

    Update reverse.cpp ...

--- reverse.cpp
+++ reverse.cpp
@@ -3,18 +3,25 @@
 #include<stdlib.h>
 #include<string.h>
 
-public class solution {
-    public int reverseBits(int n) {
-       int result = 0;
-       for(int i=0; i<32;i++)
-       {
-        result <<= 1;
-        if ((n & 1) == 1)
-           result++;
-           n >>= 1;
-        }
-        return result;
-        }
+int reverseBits(int);
 
-};
+int main() {
+   reverseBits(2546);
+  return 0;
+}
+
+int reverseBits(int n) {
+       int result = 0;
+          for(int i=0; i<32;i++) {
+            result <<= 1;
+              if ((n & 1) == 1)
+                 result++;
+                 n >>= 1;
+              }
+              return result;
+          }
+
+}
+
+
 
