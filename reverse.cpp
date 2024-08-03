5f7885dcd74dbb72e8bceb30fc674d32a084cf63
elhadyLemtai <78748781+elhadyLemtai@users.noreply.github.com>
2024-08-03 20:52:25 +0100

    no changes made

--- reverse.cpp
+++ reverse.cpp
@@ -12,16 +12,16 @@ int main() {
 
 int reverseBits(int n) {
        int result = 0;
-          for(int i=0; i<32;i++) {
+       for(int i=0; i<32;i++) {
             result <<= 1;
-              if ((n & 1) == 1)
+            if ((n & 1) == 1)
                  result++;
                  n >>= 1;
-              }
-              return result;
-          }
+            }
+            return result;
+      }
 
-}
+ }
 
 
 
